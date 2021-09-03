f13ec45c3649b4d337d8e5e7c3abb6a3fbf9cd4c
Hamzammia <mahmudul.web.developer@gmail.com>
2021-09-03 14:29:06 +0600

    add

---
+++ .vscode/app.js
@@ -0,0 +1,15 @@
+console.time('my code took time')
+console.log("hello")
+
+console.log(4 + 32);
+console.log(true);
+
+console.log([21, 2, 1, 545, 47]);
+console.log({ hamza: 'this', marks: 28 });
+console.log([121, 445, 7, 5, 14, 5]);
+console.table({ this: "hamza", markes: 28 });
+console.warn("warning");
+// console.clear();
+
+
+console.timeEnd('my code took time')
---
+++ .vscode/tut3.js
@@ -0,0 +1,25 @@
+var name = 'hamza';
+name = 'jahid';
+var channel;
+channel = "easy learn"
+var marks = "345";
+var city = "dhaka"
+console.log(name, channel, marks, city);
+
+
+const myname = 'hamza';
+// myname = 'jahid'
+console.log(myname)
+
+{
+    //let city = ' barahmanbaria';
+    city = 'bijoynagar';
+    console.log(city);
+}
+console.log(city);
+
+
+const arr1 = [12, 1, 4, 5];
+// arr1 = [10, 2, 0]
+arr1.push(14);
+console.log(arr1)
